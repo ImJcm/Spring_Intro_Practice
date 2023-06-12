cf4133987d3958fafafdae64a91a696848076ee9
ImJcm <wjsckdals0909@naver.com>
2023-06-12 20:15:15 +0900

    init setting

---
+++ src/main/java/com/example/spring_intro_practice/SpringIntroPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_intro_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntroPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIntroPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_intro_practice/SpringIntroPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_intro_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntroPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
